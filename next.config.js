1b837697995aeea531d0ea1d0ecf29dc6c8bdfa7
weekendfish <yusen356@gmail.com>
2025-03-06 01:28:23 +0000

    up

--- next.config.js
+++ next.config.js
@@ -7,8 +7,10 @@ let basePath = ''
 if (isGithubActions) {
     // trim off `<owner>/`
     const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-    assetPrefix = `/${repo}/`
-    basePath = `/${repo}`
+    // assetPrefix = `/${repo}/`
+    // basePath = `/${repo}`
+    assetPrefix = `/`
+    basePath = `/`
 }
 
 const nextConfig = {
