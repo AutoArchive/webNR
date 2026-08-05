6509d2ea90346ed977eb08d43e19e644d92aaf09
Sophia Yu <194454896+weekendfish@users.noreply.github.com>
2026-08-05 11:31:57 -0700

    fix: restore public GA4 configuration

--- config/constants.ts
+++ config/constants.ts
@@ -6,6 +6,10 @@ export const CONFIG = {
   HOME_PAGE: 'https://www.webnovel.win',
   CANONICAL_DOMAIN: 'https://app.webnovel.win',
 
+  ANALYTICS: {
+    GOOGLE_ANALYTICS_ID: 'G-DGH8HNQKE4',
+  },
+
   PWA: {
     THEME_COLOR: '#000000',
     ICON_192: '/icons/icon-192x192.png',
